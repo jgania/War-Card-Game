d080f654d2d4a2d6b1e9d795db8177bdf80d8ee8
jgania <jordan.gania@gmail.com>
2022-05-19 11:59:17 -0600

    Functionality Finished

--- Week 6 Coding Assignment.js
+++ Week 6 Coding Assignment.js
@@ -14,13 +14,16 @@
 
 const SUITS = ["♥", "♦", "♣", "♠"];
 const VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-
-//Assign value to cards
 const CARD_VALUE_MAP = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
 
 class Player {
     constructor(name){
         this.name = name;
+        this.score = 0;
+        this.playerDeck = [];
+    }
+    addNewDeck(deck) {
+        this.playerDeck = deck;
     }
 }
 
@@ -38,9 +41,10 @@ class Deck {
     //create a function for shuffling cards
     shuffle() {
         for (let i = 0; i < 52; i++){
+            //Math.floor creates an integer
             let randomIndex = Math.floor(Math.random() * 52);
             let oldIndex = this.cards[randomIndex]
-            this.cards[randomIndex] = cards[i];
+            this.cards[randomIndex] = this.cards[i];
             this.cards[i] = oldIndex;             
         }
     }
@@ -48,6 +52,7 @@ class Deck {
 
 //create a deck of cards
 function freshDeck() {
+    //using a flat map makes a nice and neat array rather than just map that will give you 4 seperate arrays
     return SUITS.flatMap(suit => {
         return VALUES.map(value => {
             return new Card(suit, value);
@@ -55,42 +60,49 @@ function freshDeck() {
     })
 }
 
-var deck = new Deck;
-console.log(deck);
-
-//deal shuffled cards equally to players
 function dealCards() {
-    let player1, player2
-    player1= new Deck(deck.cards.slice(0, 25))
-    player2= new Deck(deck.cards.slice(26, 51))
+    let deck1= new Deck(deck.cards.slice(0, 25));
+    let deck2= new Deck(deck.cards.slice(26, 51));
+    return deck1, deck2;
+}   
+
+function startGame(player1, player2) {
+    const deck = new Deck();
+    deck.shuffle();
+    deck.dealCards();
+    player1.addNewDeck(deck1);
+    player2.addNewDeck(deck2);
 }
 
-
-
-function startGame() {
-    //chose card from each player in each player deck
-    //create loop to subtract card from player deck and 
-    //assign point to the larger card value
+function displayRoundResults(player1, player2, roundNum) {
+    console.log(`${player1.name} plays: ${player1.playerDeck[roundNum].value} of ${player1.playerDeck[roundNum].suit}
+    `);
+    console.log(`${player2.name} plays: ${player2.playerDeck[roundNum].value} of ${player2.playerDeck[roundNum].suit}
+    `);
 }
 
-//build out point system
-
-function isWinner(playerOneCard, playerTwoCard) {
-    if(CARD_VALUE_MAP[playerOneCard, value] > CARD_VALUE_MAP[playerTwoCard, value]) {
-        
-    } else if (CARD_VALUE_MAP[playerOneCard, value] < CARD_VALUE_MAP[playerTwoCard, value]) {
-        
-    } else {
-
+function executeRound(player1, player2) {
+        for (let i = 0; i < player1.playerDeck.length; i++) {
+        displayRoundResults(player1, player2, i);
+        if (CARD_VALUE_MAP[player1.playerDeck[i].value] > CARD_VALUE_MAP[player2.playerDeck[i].value]) {
+            player1.score += 1;
+            console.log(`${player1.name} has won this round`);
+      } else if (CARD_VALUE_MAP[player1.playerDeck[i].value] < CARD_VALUE_MAP[player2.playerDeck[i].value]) {
+            player2.score += 1;
+            console.log(`${player2.name} has won this round`);
+      } else {
+            console.log(`Both ${player1} and ${player2} have tied. No points rewarded`);
+      }
     }
-
 }
 
-// Menu class 
-// Prompt user to rename player 1 and player 2
-// Create new shuffled deck
-// divide shuffled cards between two players
-// start game
-// enterate thsou the game 26 rounds and assign points
-// display winner
-// prompt to exit or start new game
+function declareWinner(player1, player2) {
+    if (player1.score > player2.score) {
+        console.log(`${player1.name} has won with a final score of: ${player1.score}`);
+    } else if (player1.score < player2.score) {
+        console.log(`${player2.name} has won with a final score of: ${player2.score}`);
+    } else {
+        console.log(`${player1.name} and ${player2.name} have tied!`);
+    }
+} 
+
